59d1047f529c0cd29878685c50fa52a1f077c846
Chris Nielson <ctniel1@gmail.com>
2018-08-29 23:14:39 -0600

    basic layout added

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
         component: DashboardComponent,
     },
     {
-        path: '/bingo',
+        path: 'bingo',
         component: BoardComponent,
     }
 ];
--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
+  board = '';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  determineBoard(option) {
+    if (option == 1) {
+      /*YSA Board*/
+      this.board = 'YSA';
+    } else if (option == 2) {
+      /*Newly-Married Board*/
+      this.board = 'Married';
+    } else if (option == 3) {
+      /*Family Board*/
+      this.board = 'Family';
+    } else {
+      /*reset*/
+      this.board = '';
+    }
   }
 
 }
