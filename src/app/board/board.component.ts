9c44f6ba9f5ba984dd92abffa1d001324df78d32
Chris Nielson <ctniel1@gmail.com>
2018-09-01 15:37:45 -0600

    Bingo board changes color when clicked

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -21,16 +21,49 @@ export class BoardComponent implements OnInit {
     if (option == 1) {
       /*YSA Board*/
       this.board = 'YSA';
+      for (var i = 0; i < this.ysaBingo.length; i++) {
+        this.ysaBingo[i] = {active: false};
+      }
     } else if (option == 2) {
       /*Newly-Married Board*/
       this.board = 'Married';
+      for (var i = 0; i < this.marriedBingo.length; i++) {
+        this.marriedBingo[i] = {active: false};
+      }
     } else if (option == 3) {
       /*Family Board*/
       this.board = 'Family';
-    } else {
+      for (var i = 0; i < this.familyBingo.length; i++) {
+        this.familyBingo[i] = {active: false};
+      }
+} else {
       /*reset*/
       this.board = '';
     }
   }
+
+  activateSquare(selection) {
+    this.ysaBingo[selection].active = true;
+    this.marriedBingo[selection].active = true;
+    this.familyBingo[selection].active = true;
+  }
+
+  deactivateSquare(selection) {
+    this.ysaBingo[selection].active = false;
+    this.marriedBingo[selection].active = false;
+    this.familyBingo[selection].active = false;
+  }
+
+  resetBoard() {
+    for (var i = 0; i < this.ysaBingo.length; i++) {
+      this.ysaBingo[i].active = false;
+    }
+    for (var i = 0; i < this.marriedBingo.length; i++) {
+      this.marriedBingo[i].active = false;
+    }
+    for (var i = 0; i < this.familyBingo.length; i++) {
+      this.familyBingo[i].active = false;
+    }
+}
 
 }
